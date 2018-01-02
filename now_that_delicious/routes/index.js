d12dc0fcedc3b1276d8a0071697da7b344e2dfc0
Jerry Lau <jerryzlau@gmail.com>
2018-01-01 17:12:10 -0800

    wes bos has a very interesting way to management his helper methods

--- now_that_delicious/routes/index.js
+++ now_that_delicious/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  // res.send('Hey! It works!');
 
   // send json as res
   // const data = {
@@ -15,6 +15,11 @@ router.get('/', (req, res) => {
   // grab query from url
   // query format ?name=jerry&age=23
   // res.json(req.query);
+
+  // render pug template 
+  res.render('hello', {
+    title: 'I love food'
+  });
 });
 
 router.get('/reverse/:name', (req, res) => {
